11f3ca412a6cd7ba202c9f4b5c32e29df90b8d6b
farman <farman@kmindz.in>
2022-08-15 23:05:55 +0530

    fixed

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const connectDB = require("./config/db")
 const app = express();
+const path = require('path')
 
 connectDB();
 
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {res.send('server running...')});
+// app.get('/', (req, res) => {res.send('server running...')});
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//serve static assets in production
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname
+            , 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
