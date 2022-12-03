1fc11b31c2dc8e5279d5803a82134cc7fbe44faf
farman <farman@kmindz.in>
2022-12-03 22:58:38 +0530

    fixed

--- server.js
+++ server.js
@@ -19,11 +19,14 @@ app.use("/api/posts", require("./routes/api/posts"));
 //serve static assets in production
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname
-            , 'frontend', 'build', 'index.html'))
-    })
+    // app.use(express.static('frontend/build'))
+    // app.get('*', (req, res) =>
+    //  {res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    // })
+    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
 }
 
 const PORT = process.env.PORT || 5000;
